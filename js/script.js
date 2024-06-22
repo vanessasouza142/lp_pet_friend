bfabde4ab77f6fabb44de606ca314f40870dbfe5
vanessasouza142 <vanessa.kbs16@gmail.com>
2024-06-21 21:59:00 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita comportamento padrão de envio do formulário (nova requisição HTTP e recarga da página)
+
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const mensagem = document.getElementById('motivo').value;
+
+    // Armazenar no Local Storage
+    localStorage.setItem('nome', nome);
+    localStorage.setItem('email', email);
+    localStorage.setItem('motivo', mensagem);
+
+    alert('Dados armazenados com sucesso!');
+
+    // Limpar os campos do formulário
+    document.getElementById('nome').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('motivo').value = '';
+  });
+
+});
