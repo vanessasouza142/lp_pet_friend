6c817b0599abd2b62f06ba4a27946645e1b14eac
vanessasouza142 <vanessa.kbs16@gmail.com>
2024-07-31 20:43:52 -0300

    WIP landing page improvements

--- js/script.js
+++ js/script.js
@@ -22,3 +22,12 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const menuToggle = document.getElementById('menu-toggle');
+  const navUl = document.querySelector('nav ul');
+
+  menuToggle.addEventListener('click', function() {
+    navUl.classList.toggle('active');
+  });
+});
